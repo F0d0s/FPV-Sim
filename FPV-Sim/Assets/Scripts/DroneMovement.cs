be60e58806ee661860c622f3857f6ad306e2a61e
Fanda Simek <fanca@fabob.net>
2023-04-12 11:37:57 +0200

    Added drone rotations
    Drone now fully moves by correct inputs.

--- FPV-Sim/Assets/Scripts/DroneMovement.cs
+++ FPV-Sim/Assets/Scripts/DroneMovement.cs
@@ -4,46 +4,51 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Threading;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
 using Vector2 = System.Numerics.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
 
 public class DroneMovement : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Rigidbody _rb;
     private float throttle;
     private float roll;
-    private float tStrength = 20;
+    private float pitch;
+    private float yaw;
+    [SerializeField] private float tSens = 20;
+    [SerializeField] private float ySens = 20;
+    [SerializeField] private float rSens = 20;
+    [SerializeField] private float pSens = 20;
 
     public InputActions inputActions;
-    // Start is called before the first frame update
+
+    private Quaternion droneQuaternion;
+    
     private void Awake()
     {
         inputActions = new InputActions();
         inputActions.Drone.Enable();
-        rb = gameObject.GetComponent<Rigidbody>();
+        _rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        roll = inputActions.Drone.Roll.ReadValue<float>();
-        throttle = inputActions.Drone.Throttle.ReadValue<float>();
-        
-        
-        
+        InputGather();
     }
 
     private void FixedUpdate()
     {
-        rb.AddRelativeForce(0,throttle * tStrength, 0);
+        droneQuaternion = Quaternion.Euler(pitch * pSens * Time.deltaTime, yaw * ySens * Time.deltaTime, roll * rSens * Time.deltaTime);
+        _rb.AddRelativeForce(0,throttle * tSens, 0);
+        _rb.MoveRotation(_rb.rotation * droneQuaternion);
     }
 
-    private void Throttle()
+    private void InputGather()
     {
-        
+        roll = inputActions.Drone.Roll.ReadValue<float>();
+        pitch = inputActions.Drone.Pitch.ReadValue<float>();
+        yaw = inputActions.Drone.Yaw.ReadValue<float>();
+        throttle = inputActions.Drone.Throttle.ReadValue<float>();
     }
 }

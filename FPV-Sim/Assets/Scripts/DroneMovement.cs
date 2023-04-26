ba0979fe80994fa544899b46db6a36b3bcab075a
Fanda Simek <fanca@fabob.net>
2023-04-26 11:38:08 +0200

    Added ActualRates
    Sensitivity is now configurable the same like in Betaflight - ActualRates

--- FPV-Sim/Assets/Scripts/DroneMovement.cs
+++ FPV-Sim/Assets/Scripts/DroneMovement.cs
@@ -11,22 +11,36 @@ using Vector3 = UnityEngine.Vector3;
 
 public class DroneMovement : MonoBehaviour
 {
-    //Input and sensitivity values
+    //Input and throttle power
     private float throttle;
     private float roll;
     private float pitch;
     private float yaw;
-    [SerializeField] private float tSens = 20;
-    [SerializeField] private float ySens = 20;
-    [SerializeField] private float rSens = 20;
-    [SerializeField] private float pSens = 20;
-
+    [SerializeField] private float throttlePower = 20;
+    
+    //Other
     private Rigidbody _rb;
-
     public InputActions inputActions;
 
-    private Quaternion droneQuaternion;
+    //Rate values
+    [Header("Pitch rates")]
+    [SerializeField]private float maxRatePitch = 720;
+    [SerializeField]private float centerSensitivityPitch = 150;
+    [SerializeField]private float expoPitch = 0.1f;
+    [Header("Yaw rates")]
+    [SerializeField]private float maxRateYaw = 720;
+    [SerializeField]private float centerSensitivityYaw = 150;
+    [SerializeField]private float expoYaw = 0.1f;
+    [Header("Roll rates")]
+    [SerializeField]private float maxRateRoll = 720;
+    [SerializeField]private float centerSensitivityRoll = 150;
+    [SerializeField]private float expoRoll = 0.1f;
 
+    private float outputPitch;
+    private float outputYaw;
+    private float outputRoll;
+    
+    //"Ground effect" values and things
     private float groundEffect;
     private float groundEffectDistance;
     private RaycastHit groundEffectHit;
@@ -52,17 +66,19 @@ public class DroneMovement : MonoBehaviour
         {
             groundEffect = 1;
         }
-
+        
+        outputPitch = GetBetaflightActualRate(maxRatePitch, centerSensitivityPitch, expoPitch, pitch);
+        outputYaw = GetBetaflightActualRate(maxRateYaw, centerSensitivityYaw, expoYaw, yaw);
+        outputRoll = GetBetaflightActualRate(maxRateRoll, centerSensitivityRoll, expoRoll, roll);
     }
 
     private void FixedUpdate()
     {
-        _rb.AddRelativeForce(0,throttle * tSens * groundEffect, 0);
+        _rb.AddRelativeForce(0,throttle * throttlePower * groundEffect, 0);
         _rb.AddRelativeTorque(
-            pitch * pSens * 6.75f * Time.deltaTime,
-            yaw * ySens * 6.75f * Time.deltaTime,
-            roll * rSens * 6.75f * Time.deltaTime);
-        
+            outputPitch * 6.75f * Time.deltaTime,
+            outputYaw * 6.75f * Time.deltaTime,
+            outputRoll * 6.75f * Time.deltaTime);
     }
 
     private void InputGather()
@@ -71,5 +87,14 @@ public class DroneMovement : MonoBehaviour
         pitch = inputActions.Drone.Pitch.ReadValue<float>();
         yaw = inputActions.Drone.Yaw.ReadValue<float>();
         throttle = inputActions.Drone.Throttle.ReadValue<float>();
+    }
+    
+    public float GetBetaflightActualRate(double maxRate, double centerSensitivity, double expo, double input)
+    {
+        double absInput = Math.Abs(input);
+        double inputSign = Math.Sign(input);
+        double expoFactor = absInput * (Math.Pow(absInput, 5) * expo + absInput * (1 - expo));
+        double rateCurve = (centerSensitivity * absInput) + ((maxRate - centerSensitivity) * expoFactor);
+        return (float)(rateCurve * inputSign);
     }
 }
